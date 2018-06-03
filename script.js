67d187122beb4aad1cb62823b48891f431c463fe
Jenna Pinkham <pinkhamjenna@gmail.com>
2018-06-03 21:11:54 +0000

    introduces hsl colorspace to better align with human color perception

--- script.js
+++ script.js
@@ -92,21 +92,28 @@ $(document).ready(function(){
     
     // looks to see if the player won
     function isThatAWin(checkColor){
-        console.log(goalColor);
-        console.log(checkColor)
+        var hslGoal = rgb2hsl(...goalColor);
+        var hslCheck = rgb2hsl(...checkColor);
+        console.log("goal: " + hslGoal.join(" "));
+        console.log("check: " + hslCheck.join(" "));
         var check = [];
+        
         for (i = 0; i < 3; i++){
-            check[i] = goalColor[i]-4 <= checkColor[i] && checkColor[i] <= goalColor[i]+4;
+            if (i = 1) {
+                check[i] = hslGoal[i] + 5 <= hslCheck[i] && hslCheck[i] <= hslGoal[i] + 5;
+            } else {
+                check[i] = hslGoal[i] - .05 <= hslCheck[i] && hslCheck[i] <= hslGoal[i] + .05;
+            }
         }
         console.log(check);
-        console.log(check.filter(x => x).length);
-        if(check.filter(x => x).length == 3) {
+        //console.log(check.filter(x => x).length);
+        /*if(check.filter(x => x).length == 3) {
             console.log('success!');
         } else if (check.filter(x => x).length ==2) {
             console.log('soooooo close!');
         } else if (check.filter(x => x).length == 1) {
             console.log('getting there!');
-        }
+        }*/
     }
     
     // allows the player to reset the mixer bottle
@@ -114,4 +121,69 @@ $(document).ready(function(){
         changeColor(mixerBottle, rgbify[169,213,224]);
         mixerBottle.addClass('empty');
     });
+    
+    //converts rgb colors to hsl
+    function rgb2hsl(r, g, b) {
+        // https://www.rapidtables.com/convert/color/rgb-to-hsl.html
+        var r0 = r/255;
+        var g0 = g/255;
+        var b0 = b/255;
+        
+        var cMax = Math.max(r0,g0,b0);
+        var cMin = Math.min(r0,g0,b0);
+        var delta = cMax - cMin;
+        
+        var hsl = [];
+        var l = (cMax + cMin)/2;
+        hsl[2] = l;
+        var s = delta / (1 - Math.abs(2*l - 1));
+        hsl[1] = s;
+        
+        var h = null;
+        if (delta == 0) {
+            h = 0;
+        } else if (cMax == r0) {
+            h = ((g0 - b0) / delta) % 6;
+        } else if (cMax == g0) {
+            h = 2 + (b0 - r0) / delta;
+        } else if (cMax == b0) {
+            h = 4 + (r0 - g0) / delta;
+        }
+        
+        h = h * 60;
+        if (h < 0) h = h + 360;
+        hsl[0] = Math.round(h);
+
+        return hsl.map(x => parseFloat(x.toFixed(2)));
+    }
+    
+    //converts hsl colors to rgb
+    function hsl2rgb(h, s, l){
+        // https://www.rapidtables.com/convert/color/hsl-to-rgb.html
+        var rgb = [];
+        var c = (1 - Math.abs(2*l - 1)) * s;
+        var x = c * (1 - Math.abs(((h/60) % 2) - 1));
+        var m = l - c/2;
+        
+        if(0 <= h && h < 60){
+            rgb = [c, x, 0];
+        } else if(60 <= h && h < 120) {
+            rgb = [x, c, 0];
+        } else if(120 <= h && h < 180){
+            rgb = [0, c, x];
+        } else if(180 <= h && h < 240){
+            rgb = [0, x, c];
+        } else if(240 <= h && h < 300){
+            rgb = [x, 0, c];
+        } else if(300 <= h && h < 360){
+            rgb = [c, 0, x];
+        }
+        
+        return rgb.map(x => Math.round(255 * (x + m)));
+    }
+    
+    // turns h,s,l into hsl(x,y%,z%)
+    function hslify(h, s, l){
+        return 'hsl(' + h + ',' + s*100 + '%,' + l*100 + '%)';
+    }
 });
